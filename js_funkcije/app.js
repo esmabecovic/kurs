46abcfc27497930c782d245aa3714b57cd45f2fa
esmabecovic <becovicesma@gmail.com>
2023-01-29 14:33:56 +0100

    first steps with functions in js

---
+++ js_funkcije/app.js
@@ -0,0 +1,32 @@
+// function kvadratOdPet() {
+//     return 5*5;
+// }
+// const rezultat = kvadratOdPet();
+// console.log(rezultat);
+//  function kvadratBroja(n) {
+//     const result = n*n;
+//     return result;
+//  }
+
+//  const rezultat = kvadratBroja(125);
+//  console.log(rezultat);
+
+// console.log(kvadratBroja(125));
+
+
+
+// function greeting (){
+//     const userName = prompt("Enter your name:");
+//     const upperCaser = userName.toUpperCase();
+//     console.log("Hello", upperCaser)
+// }
+// greeting();
+
+
+// const ime = "Esma";
+// const city = "Novi Pazar";
+// const age = 21;
+
+// console.log(`Moje ime je ${ime}, imam ${age}, iz ${city}`);
+
+
